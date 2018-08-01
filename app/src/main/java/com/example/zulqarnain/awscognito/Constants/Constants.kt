a1c82a86302af271fcffa7cb4bf23061383c14a0
Zulqarnain <zuluparvi@gmail.com>
2018-08-01 10:46:07 +0500

    Update Constants.kt

--- app/src/main/java/com/example/zulqarnain/awscognito/Constants/Constants.kt
+++ app/src/main/java/com/example/zulqarnain/awscognito/Constants/Constants.kt
@@ -11,9 +11,9 @@ class Constants private constructor() {
         const val STATIC_MOVIES_BASE_URL = "https://"
 
         const val USER_POOL_ID = "us-east-1_C5VoGDmik"
-        const val APP_CLIENT_ID = "bjgplsvn6neuhn3erlovrcro3"
-        const val ACCESSS_KEY = "AKIAJR3WSRNWYQAW25OA"
-        const val SECRETE_KEY = "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"
+        const val APP_CLIENT_ID = "******
+        const val ACCESSS_KEY = ""******"
+        const val SECRETE_KEY = ""******"******"
         val COGNITO_REGION = Regions.US_EAST_1
         val APP_SECRET = null
     }
